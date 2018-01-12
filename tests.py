4de994c824fac3bcf36e2e1bae89e0ee8b2b7ba6
Oscar Triano dotoscat <user@example.com>
2018-01-12 18:54:22 +0100

    Add tests

---
+++ tests.py
@@ -0,0 +1,53 @@
+#  Copyright (C) 2018 Oscar 'dotoscat' Triano
+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+from polytanks.ogf4py_toyblock3 import system, component
+
+class Entity:
+    Body = component.Body
+    def __init__(self):
+        self.body = self.Body()
+
+class SystemTest(unittest.TestCase):
+    def setUp(self):
+        self.entity = Entity()
+        self.physics = system.PhysicsSystem()
+        self.physics.add_entity(self.entity)
+
+    def test_Physics_nocollision(self):
+        entity = self.entity
+        entity.body.vel_x = 16
+        entity.body.vel_y = 16
+        self.physics(1., 0.)
+        self.assertTrue(entity.body.x >= 16., "Move x")
+        self.assertTrue(entity.body.y >= 16., "Move y")
+
+    def test_Physics_collision(self):
+        entity = self.entity
+        entity.body.vel_x = 16.
+        entity.collision = []
+        one = component.CollisionRect(32., 32.)
+        one.x = 1.
+        one.y = 1.
+        self.assertEqual(one.top, 33., "top")
+        self.assertEqual(one.right, 33., "right")
+        two = component.CollisionRect(8., 8.)
+        two.offset = (-4, 0.)
+        entity.collision.append(one)
+        entity.collision.append(two)
+        self.physics(1., 0.)
+        self.assertEqual(one.x, 16., "one x")
+        self.assertEqual(two.x, 16. + -4., "two x with offset")
