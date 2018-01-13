259cc9d7b0ec9514e5bdf25670a5d6663612b014
Oscar Triano dotoscat <user@example.com>
2018-01-13 13:42:19 +0100

    Start to show graphics

--- client.py
+++ client.py
@@ -1,5 +1,22 @@
 import pyglet
 from polytanks.ogf4py.director import Director
+from polytanks.ogf4py.scene import Scene
+from polytanks import assets
+
+class Screen(Scene):
+    def __init__(self):
+        super().__init__(1)
+        self.sprite = pyglet.sprite.Sprite(assets.images["tank-base"], batch=self.batch)
+
+    def init(self):
+        pass
+
+    def quit(self):
+        pass
+
+    def update(self, dt):
+        print("dt", dt)
 
 director = Director()
+director.scene = Screen()
 pyglet.app.run()
