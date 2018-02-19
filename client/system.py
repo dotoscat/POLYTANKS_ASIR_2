02b0c71767905b9d8cd33d5facbf039ffdbea28b
Oscar Triano dotoscat <user@example.com>
2018-02-19 17:33:57 +0100

    Guardar SpriteSystem

---
+++ client/system.py
@@ -0,0 +1,8 @@
+import toyblock3
+
+class SpritesSystem(toyblock3.System):
+    def _update(self, entity):
+        body = entity.body
+        sprite = entity.sprite
+        sprite.x = body.x
+        sprite.y = body.y
