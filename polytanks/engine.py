f1506a9331ef4ffa780ab455546e5b600940375b
Oscar Triano dotoscat <user@example.com>
2018-02-21 13:44:54 +0100

    Mirar si el id es un entero

---
+++ polytanks/engine.py
@@ -0,0 +1,31 @@
+class AbstractEngine:
+    def __init__(self, pools):
+        types = ("player",)
+        for t in types:
+            if t not in pools:
+                raise Exception("{} not found in pools".format(t))
+        self.pools = pools
+        self.entities = {}
+    
+    def add_player(self, id=None):
+        player_pool = self.pools.get("player")
+        player = player_pool()
+        if player is None:
+            return 
+        id = id if isinstance(id, int) else self.generate_id()
+        self.entities[id] = player
+        return (id, player)
+
+    def remove(self, id):
+        entity = self.entities.get(id)
+        if not entity:
+            return False
+        entity.free()
+        del self.entities[id]
+        return True
+
+    def update(self, dt):
+        raise NotImplementedError
+
+    def generate_id(self):
+        raise NotImplementedError
