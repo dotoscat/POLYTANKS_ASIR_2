60aaddb7b0ad4ba385e4e926577245341b72e13b
Oscar Triano dotoscat <user@example.com>
2018-03-03 13:16:19 +0100

    Colocar las licencias que faltan

--- server/protocol.py
+++ server/protocol.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017  Oscar 'dotoscat' Triano <dotoscat (at) gmail (dot) com>
+# Copyright (C) 2018  Oscar 'dotoscat' Triano <dotoscat (at) gmail (dot) com>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
