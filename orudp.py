e32d61e647076b85eaead4b4b110cd30c882928f
Oscar Triano dotoscat <user@example.com>
2018-03-12 18:04:19 +0100

    Separar clases a Mailbox y Postbox

--- orudp.py
+++ orudp.py
@@ -15,46 +15,16 @@ class Message:
         self.event = None
         self.address = None
 
-class Office:
+class Mailbox:
     def __init__(self, socket):
         self._messages = {}
         self._id = 0
         self._socket = socket
         self._mysched = sched.scheduler()
         self._select = selectors.DefaultSelector()
-        self._select.register(socket, selectors.EVENT_READ, self._recv)
 
     def __del__(self):
         self._select.close()
-
-    def send_message(self, data, time_for_response, tries=None, address=None):
-        buffer = header.pack(self._id, DATA) + data
-        message = Message(self._id, buffer) 
-        message.tries = tries
-        message.address = address
-        message.event = self._mysched.enter(time_for_response, 1, self._resend, argument=(message, time_for_response))
-        self._messages[self._id] = message
-        self._id += 1
-        return self._send(buffer, address)
-
-    def _send(self, data, address=None):
-        if address:
-            return self._socket.sendto(data, address)
-        else:
-            return self._socket.send(data)
-
-    def _resend(self, message, time_for_response):
-        if message.tries == 0:
-            return
-        print("resend after {} seconds".format(time_for_response))
-        self._send(message.data, message.address)
-        print("resend", message.data)
-        self._mysched.enter(time_for_response, 1, self._resend, argument=(message, time_for_response))
-        if message.tries:
-            message.tries -= 1
-
-    def _recv(self, socket):
-        pass
 
     def run(self):
         events = self._select.select(0)
@@ -80,3 +50,30 @@ class Office:
 
     def empty(self):
         return self._mysched.empty()
+
+class Postbox(Mailbox):
+    def send_message(self, data, time_for_response, tries=None, address=None):
+        buffer = header.pack(self._id, DATA) + data
+        message = Message(self._id, buffer) 
+        message.tries = tries
+        message.address = address
+        message.event = self._mysched.enter(time_for_response, 1, self._resend, argument=(message, time_for_response))
+        self._messages[self._id] = message
+        self._id += 1
+        return self._send(buffer, address)
+
+    def _send(self, data, address=None):
+        if address:
+            return self._socket.sendto(data, address)
+        else:
+            return self._socket.send(data)
+
+    def _resend(self, message, time_for_response):
+        if message.tries == 0:
+            return
+        print("resend after {} seconds".format(time_for_response))
+        self._send(message.data, message.address)
+        print("resend", message.data)
+        self._mysched.enter(time_for_response, 1, self._resend, argument=(message, time_for_response))
+        if message.tries:
+            message.tries -= 1
