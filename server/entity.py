15e82ce7abf077a0a487e6af5e403a90e7239372
Oscar Triano dotoscat <user@example.com>
2018-03-15 10:16:17 +0100

    Anyadir modulo entity.py

---
+++ server/entity.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2018  Oscar 'dotoscat' Triano    <dotoscat (at) gmail (dot) com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from polytanks import entity
+from polytanks.system import physics, collision
+from .system import input
+
+class Player(entity.Player):
+    SYSTEMS = (input, physics, collision)
+
+class Platform(entity.Platform):
+    SYSTEMS = (collision,)
+
+class Bullet(entity.Bullet):
+    SYSTEMS = (physics, collision)
