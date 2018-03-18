dbddbf06c67bf971ada971eba441fa13f926b856
Oscar Triano dotoscat <user@example.com>
2018-03-18 12:15:04 +0100

    Mirar si la ultima bala pasa de la colision

--- tests_game.py
+++ tests_game.py
@@ -14,18 +14,23 @@ ManagedBlastzone()
 
 class TestBullets(unittest.TestCase):
     def test_bullets_freed_in_blastzone(self):
+        self.collided = False
         self.ManagedBullet = toyblock3.Manager(server.entity.Bullet, 3)
-        bullet = self.ManagedBullet()
+        last_bullet = self.ManagedBullet()
         polytanks.system.collision.register_callbacks(
             (polytanks.collision.BULLET, polytanks.collision.BLAST_ZONE),
             start=self._bullet_blastzone
         )
         self._run_once()
+        bullet = self.ManagedBullet()
+        self._run_once()
 
     def _run_once(self):
         polytanks.system.collision()
-        self.assertEqual(len(self.ManagedBullet.pool.used), 0, "Bullet not freed!")
+        self.assertTrue(self.collided and len(self.ManagedBullet.pool.used) == 0, "Bullet is not freed!")
+        self.collided = False
 
     def _bullet_blastzone(self, bullet, blastzone, bullet_rect, blastzone_rect):
         print("Bom!")
         bullet.free()
+        self.collided = True
